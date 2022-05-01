dc15129d51c20b093d858fd660d53f76b14aa977
Matheus Pedroni <pnmatheus@protonmail.com>
2022-05-01 18:59:27 -0300

    moving streaming to helpers

--- scripts/stream_data.py
+++ scripts/stream_data.py
@@ -4,64 +4,25 @@ Provide the MAC address of the MetaWear device as the --address argument.
 """
 
 import argparse
-import multiprocessing as mp
-from threading import Event
 
-
-from src.data_writer import DataWriter
-from src.device import MetaWearDevice
-from src.helpers import consume_data_queue
-from src.logger import LOG
-from src.streamer import DataStreamer
+from src.cli import CLI
+from src.helpers import stream_data
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Stream sensor data from MetaWear device.')
-    parser.add_argument('--address', type=str, help='MetaWear device MAC address.', required=True)
-    parser.add_argument('--raw', action='store_true', help='Stream raw sensor data.')
-    parser.add_argument('--max-queue-size', type=int, help='Maximum queue size.', default=2500)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--max-queue-size", type=int, default=25000, help="Maximum number of data points to store in queue")
     args = parser.parse_args()
-
-    device = MetaWearDevice(args.address)
-    device.connect()
-    # TODO configure sensors
-
-    acc_queue = mp.Queue(args.max_queue_size)
-    gyro_queue = mp.Queue(args.max_queue_size)
-    acc_queue_event = mp.Event()
-    gyro_queue_event = mp.Event()
-
-    main_event = Event()
-    acc_writer = DataWriter("data/acc_data.csv", "(g/s)", raw=args.raw)
-    gyro_writer = DataWriter("data/gyro_data.csv", "(rad/s)", raw=args.raw)
-    streamer = DataStreamer(device, acc_queue, gyro_queue, raw=args.raw)
-    streamer.subscribe_to_sensors()
-
-    stream_process = mp.Process(target=streamer.start_streaming())
-    acc_write_process = mp.Process(target=consume_data_queue, args=(acc_queue_event, acc_queue, acc_writer))
-    gyro_write_process = mp.Process(target=consume_data_queue, args=(gyro_queue_event, gyro_queue, gyro_writer))
-    stream_process.start()
-    acc_write_process.start()
-    gyro_write_process.start()
-    try:
-        main_event.wait()
-    except KeyboardInterrupt:
-        print()
-        LOG.info('Keyboard interrupt received. Stopping stream.')
-        if stream_process.is_alive():
-            stream_process.close()
-        streamer.stop_streaming()
-        LOG.info("waiting for write process to finish, please wait...")
-        acc_queue.close()
-        gyro_queue.close()
-        acc_queue_event.wait(5)
-        gyro_queue_event.wait(5)
-        acc_write_process.kill()
-        gyro_write_process.kill()
-        acc_writer.close()
-        gyro_writer.close()
-        LOG.info("exiting")
-        main_event.set()
+    cli = CLI('data/metadata.db')
+    user_data = cli.log_in()
+    start = input("Do you want to start a session? (y/n) ")
+    if start not in 'Yy':
+        exit(0)
+    print("Starting session...")
+    session_id = cli.start_session(user_data['user_id'], user_data['device_id'])
+    print(user_data['device_config']['acc_frequency'])
+    stream_data(user_data['device'], session_id, args.max_queue_size, args.max_queue_size)
 
 
 if __name__ == "__main__":
--- src/helpers.py
+++ src/helpers.py
@@ -4,12 +4,15 @@ Miscelaneous helper functions.
 
 import queue
 import signal
-from multiprocessing import Queue
+import multiprocessing as mp
 from multiprocessing.synchronize import Event
+import threading
 import time
 
 from src.data_writer import DataWriter
+from src.device import Device
 from src.logger import LOG
+from src.streamer import DataStreamer
 
 
 def disable_control_c(func):
@@ -23,7 +26,7 @@ def disable_control_c(func):
 
 
 @disable_control_c
-def consume_data_queue(event: Event, data_queue: Queue, writer: DataWriter, sleep_time: int = 2) -> None:
+def consume_data_queue(event: Event, data_queue: mp.Queue, writer: DataWriter, sleep_time: int = 2) -> None:
     """ Consume a data queue with a writer."""
     while True:
         try:
@@ -34,3 +37,47 @@ def consume_data_queue(event: Event, data_queue: Queue, writer: DataWriter, slee
             LOG.debug("No data in queue")
             event.set()
             time.sleep(sleep_time)
+
+
+def stream_data(
+        device: Device, session_id: int, acc_max_queue_size: int, gyro_max_queue_size: int,
+        raw: bool = False) -> None:
+
+    device.connect()
+
+    acc_queue = mp.Queue(acc_max_queue_size)
+    gyro_queue = mp.Queue(gyro_max_queue_size)
+    acc_queue_event = mp.Event()
+    gyro_queue_event = mp.Event()
+
+    main_event = threading.Event()
+    acc_writer = DataWriter(f"data/session/{session_id}_acc_data.csv", "(g/s)", raw=raw)
+    gyro_writer = DataWriter(f"data/sessions/{session_id}_gyro_data.csv", "(rad/s)", raw=raw)
+    streamer = DataStreamer(device, acc_queue, gyro_queue, raw=raw)
+    streamer.subscribe_to_sensors()
+
+    stream_process = mp.Process(target=streamer.start_streaming())
+    acc_write_process = mp.Process(target=consume_data_queue, args=(acc_queue_event, acc_queue, acc_writer))
+    gyro_write_process = mp.Process(target=consume_data_queue, args=(gyro_queue_event, gyro_queue, gyro_writer))
+    stream_process.start()
+    acc_write_process.start()
+    gyro_write_process.start()
+    try:
+        main_event.wait()
+    except KeyboardInterrupt:
+        print()
+        LOG.info('Keyboard interrupt received. Stopping stream.')
+        if stream_process.is_alive():
+            stream_process.close()
+        streamer.stop_streaming()
+        LOG.info("waiting for write process to finish, please wait...")
+        acc_queue.close()
+        gyro_queue.close()
+        acc_queue_event.wait(5)
+        gyro_queue_event.wait(5)
+        acc_write_process.kill()
+        gyro_write_process.kill()
+        acc_writer.close()
+        gyro_writer.close()
+        LOG.info("exiting")
+        main_event.set()
